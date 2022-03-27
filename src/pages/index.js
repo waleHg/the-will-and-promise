ea507d4e1f76d5f65711a5a559e2d159a4bfc765
walehg <tanthum@gmail.com>
2022-03-27 15:39:39 +0200

    tried to fix tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class RootIndex extends React.Component {
         <Quote
 
           icon={bible.icon.gatsbyImageData}
-          verse={bible.content}
+          verse={bible.title}
           reference={bible.reference}
         />
 
@@ -47,7 +47,7 @@ export const pageQuery = graphql`
       filter: { contentful_id: { eq: "5dNnCoPvgLj8wquimKgHxy" } }
     ) {
       nodes {
-        content
+        title
         reference
         icon: image {
           gatsbyImageData(
