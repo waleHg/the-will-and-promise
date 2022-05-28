ddd8efdd5b2abd31d7f4a778ec71d91984cdcb98
walehg <tanthum@gmail.com>
2022-05-28 21:13:43 +0200

    fix background of nav on mobile and align

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,12 +4,12 @@ import { Link } from 'gatsby'
 import * as styles from './navigation.module.css'
 
 const Navigation = () => (
-  <nav role="navigation" className="flex flex-col md:flex-row md:justify-between py-4 gap-2 md:gap-0 pr-0 pl-8 md:pr-8 md:pl-8 lg:px-32" aria-label="Main">
+  <nav role="navigation" className="flex flex-col md:flex-row md:justify-between pt-4 pb-0 md:pt-2 md:pb-2 gap-4 md:gap-0 pr-0 pl-0 md:pr-8 md:pl-8 lg:px-32" aria-label="Main">
     <Link to="/" className="flex gap-2 mx-auto md:mx-0 my-auto">
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>The will and promise</span>
+      <span className="font-bold">The will and promise</span>
     </Link>
-    <ul className="flex flex-row gap-6 py-4 overflow-x-scroll scrollbar-hide ">
+    <ul className="flex flex-row gap-6 py-4 overflow-x-scroll scrollbar-hide bg-slate-50 md:bg-transparent pl-24 pr-4 sm:pl-auto sm:pr-auto justify-center">
     
       <li className="whitespace-nowrap">
         <Link to="/blog/" activeClassName="whitespace-nowrap">
